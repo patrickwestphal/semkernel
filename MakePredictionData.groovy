e8ea9041200ccaded9d6d8e79dfef0e547e2ddc4
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-01-20 15:12:21 +0100

    added comments and Grabs to MakePredictionData.groovy

--- MakePredictionData.groovy
+++ MakePredictionData.groovy
@@ -1,10 +1,32 @@
-import org.semanticweb.owlapi.io.* 
-import org.semanticweb.owlapi.model.*
-import org.semanticweb.owlapi.apibinding.OWLManager
-import org.semanticweb.owlapi.vocab.OWLRDFVocabulary
-import org.semanticweb.owlapi.reasoner.*
-import org.semanticweb.elk.owlapi.*
+@Grapes([
+	@Grab(group='commons-cli', module='commons-cli', version='1.2'),
+	@Grab(group='net.sourceforge.owlapi', module='owlapi-api', version='4.0.0'),
+	@Grab(group='net.sourceforge.owlapi', module='owlapi-apibinding', version='4.0.0'),
+	@Grab(group='net.sourceforge.owlapi', module='owlapi-impl', version='4.0.0'),
+	@Grab(group='net.sourceforge.owlapi', module='owlapi-parsers', version='4.0.0'),
+	@Grab(group='log4j', module='log4j', version='1.2.17'),
+	@Grab(group='org.semanticweb.elk', module='elk-owlapi', version='0.4.1'),
+	@Grab(group='org.slf4j', module='slf4j-log4j12', version='1.7.10')
+])
 
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.semanticweb.elk.owlapi.ElkReasonerFactory
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model.IRI
+import org.semanticweb.owlapi.model.OWLDataFactory
+import org.semanticweb.owlapi.model.OWLOntology
+import org.semanticweb.owlapi.model.OWLOntologyManager
+import org.semanticweb.owlapi.reasoner.ConsoleProgressMonitor
+import org.semanticweb.owlapi.reasoner.InferenceType
+import org.semanticweb.owlapi.reasoner.OWLReasoner
+import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration
+
+/* ****************************************************************************
+ * set up argument parsing and logging
+ */
 def cli = new CliBuilder()
 cli.with {
 usage: 'Self'
@@ -31,36 +53,55 @@ def fout = new PrintWriter(new BufferedWriter(new FileWriter(opt.o)))
 Double ratio = -1
 if (opt.r) { ratio = new Double(opt.r) }
 
+Logger logger = Logger.getLogger('MakePredictionData')
+logger.setLevel(Level.INFO)
+
+/* ****************************************************************************
+ * set up ontology
+ */
+
 def classifierFor = new TreeSet()
 classifierFor.add(searchClass)
 
+String mpFilePath = 'mp.obo'
+logger.info("Reading ontology file $mpFilePath ...")
 OWLOntologyManager manager = OWLManager.createOWLOntologyManager()
+OWLOntology ont = manager.loadOntologyFromOntologyDocument(new File(mpFilePath))
+logger.info('-Done-')
 
-OWLOntology ont = manager.loadOntologyFromOntologyDocument(new File("mp.obo"))
-
+logger.info('Computing class hierarchy...')
 OWLDataFactory fac = manager.getOWLDataFactory()
-
-OWLReasonerFactory reasonerFactory = null
-
 ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor()
 OWLReasonerConfiguration config = new SimpleConfiguration(progressMonitor)
-OWLReasonerFactory fac1 = new ElkReasonerFactory()
-OWLReasoner reasoner = fac1.createReasoner(ont)
+OWLReasonerFactory reasonerFactory = new ElkReasonerFactory()
+OWLReasoner reasoner = reasonerFactory.createReasoner(ont, config)
 
 reasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY)
+logger.info('-Done-')
 
+/* ****************************************************************************
+ * set up class mappings and classes to use
+ */
+logger.info("Getting subclasses of $searchClass ...")
 def cl = fac.getOWLClass(IRI.create(searchClass))
 reasoner.getSubClasses(cl, false).getFlattened().each { sc ->
   classifierFor.add(sc.toString().replaceAll("<","").replaceAll(">",""))
 }
+logger.info('-Done-')
+
 
 def map = [:].withDefault { new TreeSet() }
 
 def classes = new TreeSet()
 
-new File("gene_association.mgi").splitEachLine("\t") { line ->
+// get the file here: ftp://ftp.informatics.jax.org/pub/reports/gene_association.mgi
+String geneAssociationFilePath = 'gene_association.mgi'
+
+// build MGI ID to URL mapping (e.g. MGI:1918911 => http://purl.obolibrary.org/obo/MGI_1918911)
+logger.info("Reading MGI IDs from $geneAssociationFilePath ...")
+new File(geneAssociationFilePath).splitEachLine("\t") { line ->
   if (! line[0].startsWith("!")) {
-    def gid = line[1]
+    def gid = line[1]  // MGI ID, e.g. MGI:1918911
     def got = line[4].replaceAll(":","_")
     def evidence = line[6]
     if (evidence != "ND") {
@@ -70,9 +111,12 @@ new File("gene_association.mgi").splitEachLine("\t") { line ->
     }
   }
 }
+logger.info('-Done-')
 
+String mousePhenotypesFilePath = 'mousephenotypes.txt'
+logger.info("Reading class mappings from $mousePhenotypesFilePath ...")
 def pmap = [:].withDefault { new LinkedHashSet() }
-new File("mousephenotypes.txt").splitEachLine("\t") { line ->
+new File(mousePhenotypesFilePath).splitEachLine("\t") { line ->
   def mgiid = line[0]
   if (mgiid in map.keySet()) {
     def pid = line[1]
@@ -83,12 +127,20 @@ new File("mousephenotypes.txt").splitEachLine("\t") { line ->
     }
   }
 }
+logger.info('-Done-')
 
+/* ****************************************************************************
+ * writing results to file
+ */
+
+logger.info("Writing class mapping to file ($opt.o)...")
 // header
 fout.print("map")
 classes.each { fout.print("\t$it") }
 fout.println ("")
+logger.info('-Done-')
 
+logger.info("Writing GO classes and annotations (positive/negative example) to file ($opt.o)...")
 List negatives = []
 List positives = []
 pmap.each { k, pset ->
@@ -106,11 +158,15 @@ pmap.each { k, pset ->
     negatives.add(s)
   }
 }
+logger.info('-Done-')
+
+logger.info("Writing list of positive and negative examples' URIs to file ($opt.o)...")
 positives.each { fout.println(it) }
 Collections.shuffle(negatives)
 def posSize = positives.size()
 def cutoff = Math.round(ratio * posSize)
 
+// trim number of negative examples to fit the ratio specified in the cli args
 if (cutoff > negatives.size()) {
   ratio = -1
 }
@@ -123,5 +179,7 @@ if (ratio > 0) {
     fout.println(it)
   }
 }
+logger.info('-Done-')
+
 fout.flush()
 fout.close()
